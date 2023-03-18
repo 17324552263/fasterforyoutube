f1a6aa89b43f4eae27c87fbe942f4b03234e2b46
Matthew Yeager <Jagermeister@users.noreply.github.com>
2023-03-18 11:38:08 -0700

    5 > Embedded videos detected - Handle no-control settings to not show speed - Protect against embeds without video ids

--- src/content/extract.js
+++ src/content/extract.js
@@ -22,4 +22,16 @@
             }
         }
     });
+
+    const isPageEmbedVideo = (pathname) => pathname.indexOf('/embed/') === 0 && pathname.length > 7;
+    const isControlsAllowed = (search) => search.indexOf('controls=0') === -1;
+    if (isPageEmbedVideo(document.location.pathname)) {
+        const videoDetails = {
+            'author': document.querySelector('.ytp-title-expanded-title > a').textContent,
+            'channelId': document.querySelector("a.ytp-title-channel-logo").href.match(/\/(?:channel|c|user)\/(UC[a-zA-Z0-9_-]{22}|[a-zA-Z0-9_-]+)/)[1],
+            'videoId': document.location.pathname.match(/\/embed\/(.*)/)[1],
+            'status': isControlsAllowed(document.location.search) ? 'OK' : 'NOCONTROLS'
+        };
+        sendMessage(videoDetails);
+    }
 })();
