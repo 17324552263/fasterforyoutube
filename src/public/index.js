62c9b6d692306d90768a0cca8cedbd03329a2a9b
Matthew Yeager <Jagermeister@users.noreply.github.com>
2023-03-19 12:22:50 -0700

    6 > Show Channel Statistics on channel page

--- src/public/index.js
+++ src/public/index.js
@@ -14,20 +14,59 @@ const setPopUpBehavior = () =>
 
 const setStatisticsOverview = (channelIds, videoIds, storage) => {
     document.getElementById('nodata').style.display = channelIds.length ? 'none' : 'flex';
-    document.getElementById('channelOverview').style.display = channelIds.length ? 'flex' : 'none';
-    document.getElementById('channelStatistics').style.display = channelIds.length ? 'flex' : 'none';
-
-    document.getElementById('channelCount').innerText = channelIds.length;
-    document.getElementById('videoCount').innerText = videoIds.length;
+    document.getElementById('statisticsOverview').style.display = channelIds.length ? 'flex' : 'none';
+    document.getElementById('allStatisticsLink').style.display = channelIds.length ? 'flex' : 'none';
 
     const watchedSeconds = videoIds.reduce((p, c) => p + storage[c].watched, 0);
-    document.getElementById('hourCount').innerText = (watchedSeconds / 3600).toFixed(2);
+    document.getElementById('allHourCount').innerText = (watchedSeconds / 3600).toFixed(2);
+};
+
+const setChannelStatistics = (channelIds, videoIds, storage) => {
+    const findChannelId = (url) => {
+        const videoMatch = url.match(/\?v=(.*)/);
+        if (videoMatch?.length === 2) {
+            const videoId = videoMatch[1];
+            return channelIds.find(c => storage[c].videos.indexOf(videoId) !== -1);
+        }
+
+        const channelNameMatch = url.match(/\/@([^\/]*)/);
+        if (channelNameMatch?.length === 2) {
+            const name = channelNameMatch[1].replaceAll(' ', '').toLowerCase();
+            return channelIds.find(c => storage[c].author.replaceAll(' ', '').toLowerCase() === name);
+        }
+
+        const channelIdMatch = url.match(/\/channel\/(.*)/);
+        if (channelIdMatch?.length === 2) {
+            const id = channelIdMatch[1];
+            return channelIds.find(c => c === id);
+        }
+    };
+
+    const setChannelViewVisble = () =>
+        document.getElementById('channelStatistics').style.display = 'flex';
+
+    const updateChannelView = (channel) => {
+        document.getElementById('channelName').innerText = channel.author;
+        document.getElementById('channelVideoCount').innerText = channel.videos.length;
+        document.getElementById('channelHourCount').innerText = (channel.videos.reduce((p, c) => p + storage[c].watched, 0) / 3600).toFixed(2);
+    };
+
+    const ActiveTabQuery = { active: true, currentWindow: true };
+    chrome.tabs.query(ActiveTabQuery).then(tabs => {
+        if (tabs[0].url) {
+            const channelId = findChannelId(tabs[0].url);
+            if (channelId) {
+                setChannelViewVisble();
+                updateChannelView(storage[channelId]);
+            }
+        }
+    });
 };
 
 const setStatisticsLink = () => {
-    const container = document.getElementById('channelStatistics');
+    const container = document.getElementById('allStatisticsLink');
     const text = document.createElement('span');
-    text.innerHTML = 'Check out your&nbsp;';
+    text.innerHTML = 'See all&nbsp;';
     const link = document.createElement('a');
     link.target = '_blank';
     link.href = chrome.runtime.getURL('public/channels.html');
@@ -46,6 +85,7 @@ chrome.storage.local.get((storage) => {
     setPopUpBehavior();
     let [channelIds, videoIds] = splitChannelsAndVideos(storage);
     setStatisticsOverview(channelIds, videoIds, storage);
+    setChannelStatistics(channelIds, videoIds, storage);
     setStatisticsLink();
     setStorePageLinks();
 });
